fb0e1dacb15d130a4d992e0018fe17cf5991c0c0
fievski <fievski@yandex.ru>
2024-06-06 15:10:18 +0600

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,20 @@
+immutable_var = 1, 0.5, True, "Строка"  # Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"
+print(immutable_var)
+
+# immutable_var ([0][1]) = 15
+# Кортежи — это контейнеры, хранящие объекты в определенном порядке. В отличие от списков, кортежи неизменяемы
+
+mutable_list = [1, 0.7], False, "Строчка"
+print(mutable_list)
+mutable_list [0][1] = 86
+print(mutable_list)
+
+
+# tuple_ = 1, 2, 3, 4
+# tuple_2 = (1, 2, 3, 4)
+# tuple_3 = tuple([1, 2, 3, 4])
+# print(type(tuple_))
+# print(tuple_)
+# print(tuple_2)
+# print(tuple_3)
+# print(tuple)
